2e4c6fab0bf73ee000d91d59e2ee45b802a39c31
luuk <luuk.wester@gmail.com>
2018-05-21 16:57:02 +0000

    Added superuser to seed

--- migrations/2018-05-21-162118_initial/up.sql
+++ migrations/2018-05-21-162118_initial/up.sql
@@ -2,10 +2,9 @@
 
 CREATE TABLE public."user"
 (
-    id integer DEFAULT nextval('user_id_seq'::regclass) PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     username text NOT NULL,
     password text NOT NULL,
     is_admin boolean NOT NULL
 );
-CREATE UNIQUE INDEX user_pkey ON public."user" (id);
 CREATE UNIQUE INDEX user_name_uindex ON public."user" (username);
---
+++ migrations/2018-05-21-164649_add_admin/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+
+INSERT INTO "user" (username, password, is_admin) VALUES
+    ('luuk',
+     '$argon2i$m=4096,t=3,p=1$zQlUvfdkaDLUvhIlr7CEaw$ILJ//Nku6u02EcrvHv/GWjWgHusXRoPNORDQ8PP4yJ',
+     TRUE);
